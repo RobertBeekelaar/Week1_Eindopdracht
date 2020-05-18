c84d0f400fd264a7325a14239c9f0d650a365325
Robert Beekelaar <R.M.Beekelaar@gmail.com>
2020-05-18 13:30:50 +0200

    update

---
+++ W2D5-MovieSearcher/script.js
@@ -0,0 +1,52 @@
+const movieLocation = document.getElementById('mainlist');
+const listLocation = movieLocation.getElementsByClassName('movielist')[0];
+const buttons = document.getElementsByClassName('allbuttons')[0];
+
+movies.forEach((movie) => {
+  newLi = document.createElement('li');
+  newA = document.createElement('a');
+  newA.setAttribute('href', 'https://www.imdb.com/title/' + movie.imdbID);
+  newImg = document.createElement('img');
+  newImg.setAttribute('src', movie.Poster);
+  newLi.appendChild(newA);
+  newA.appendChild(newImg);
+  listLocation.appendChild(newLi);
+})
+
+buttonList = Array.from(buttons.getElementsByClassName('radiobutton'));
+buttonList.forEach((button) => {
+  button.addEventListener("change", () => {
+    console.log(button.value)
+
+    if (button.value !== "newest") {
+
+      const filteredMovies = movies.filter((movie) => movie.Title.includes(button.value));
+      listLocation.innerHTML = " ";
+      filteredMovies.forEach(movie => {
+        newLi = document.createElement('li');
+        newA = document.createElement('a');
+        newA.setAttribute('href', 'https://www.imdb.com/title/' + movie.imdbID);
+        newImg = document.createElement('img');
+        newImg.setAttribute('src', movie.Poster);
+        newLi.appendChild(newA);
+        newA.appendChild(newImg);
+        listLocation.appendChild(newLi);
+      })
+    } else {
+      const filteredMoviesDate = movies.filter((movie) => Number(movie.Year) >= 2014);
+      listLocation.innerHTML = " ";
+      filteredMoviesDate.forEach(movie => {
+        newLi = document.createElement('li');
+        newA = document.createElement('a');
+        newA.setAttribute('href', 'https://www.imdb.com/title/' + movie.imdbID);
+        newImg = document.createElement('img');
+        newImg.setAttribute('src', movie.Poster);
+        newLi.appendChild(newA);
+        newA.appendChild(newImg);
+        listLocation.appendChild(newLi);
+      })
+    }
+  })
+})
+
+
